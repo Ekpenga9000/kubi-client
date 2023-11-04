af299e9681bae8824846aac5f5d1d42cf687c8fd
Louis Ekpenga <louisekpenga@MBPl20FVFHHBUYQ05D.local>
2023-11-03 20:56:24 -0400

    Font colour update.

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,14 +1,17 @@
 import NavBar from "../../components/nav/NavBar";
+import "./Dashboard.scss";
 
 const Dashboard = () =>{
     return(
         <section className="dashboard">
             <NavBar/>
             <section className="dashboard__container">
-                <div className="dashboard__sidebar">side bar</div>
+                <div className="dashboard__sidebar">
+                    This would take in the sidebar component.
+                </div>
                 <div className="dashboard__main">
-                    <h2>Your Projects</h2>
-                    <div>Actual projects</div>
+                    <h2 className="dashoard__title">Your Projects</h2>
+                    <div className="dashboard__projects">Actual projects</div>
                 </div>
             </section>
         </section>

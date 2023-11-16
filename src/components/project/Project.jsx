7e2fee66a8f838acdddde4c70607a724a93d7251
Louis Ekpenga <louisekpenga@MBPl20FVFHHBUYQ05D.local>
2023-11-15 20:58:13 -0500

    Project view updated.

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -1,28 +1,38 @@
 import "./Project.scss";
 import { Link } from 'react-router-dom';
-import { AiFillFolderOpen, AiFillFolder} from 'react-icons/ai';
-import { MdOutlineDeleteOutline, MdOutlineMoreHoriz} from 'react-icons/md';
+import { AiFillFolderOpen, AiFillFolder } from 'react-icons/ai';
+import { MdOutlineDeleteOutline, MdOutlineMoreHoriz } from 'react-icons/md';
 
 
 function Project({ project }) {
-    const {id, name,projectKey, type, lead, status } = project;
-  return (
-      <div className='project'>
-          <div className='project__row'>
-              <Link to={`/projects/${id}`} className='project__title'>{name}</Link>
-              <p>{projectKey}</p>
-              <p>{type}</p>
-              <p>{lead}</p>
-              <p className={`project__status--${status.toLowerCase()}`}> {status === "Active" ? <AiFillFolderOpen /> : <AiFillFolder />} {status}</p>
-              <div className='project__btn-div'>
-                  <Link to={`/projects/${id}`} className='project__link'>
-                  <MdOutlineMoreHoriz/>
-                  </Link> 
-                  <MdOutlineDeleteOutline className='project__delete'/>
-              </div>
-          </div> 
-    </div>
-  )
+    const { id, name, projectKey, type, lead, status } = project;
+    return (
+        <div className='project'>
+            <div className='project__row'>
+                <div className="project__cell">
+                    <Link to={`/projects/${id}`} className='project__title'>{name}</Link>
+                </div>
+                <div className="project__cell">
+                    <p>{projectKey}</p>
+                </div>
+                <div className="project__cell">
+                    <p>{type}</p>
+                </div>
+                <div className="project__cell">
+                    <p>{lead}</p>
+                </div>
+                <div className="project__cell">
+                    <p className={`project__status--${status.toLowerCase()}`}> {status === "Active" ? <AiFillFolderOpen /> : <AiFillFolder />} {status}</p>
+                </div>
+                <div className='project__btn-div'>
+                    <Link to={`/projects/${id}`} className='project__link'>
+                        <MdOutlineMoreHoriz />
+                    </Link>
+                    <MdOutlineDeleteOutline className='project__delete' />
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Project;
--- src/components/project_list/ProjectList.jsx
+++ src/components/project_list/ProjectList.jsx
@@ -4,18 +4,28 @@ import "./ProjectList.scss"
 
 function ProjectList({ projectList }) {
   return (
-      <div className='projectList'>
-        <div className='projectList__title-div'>
+    <div className='projectList'>
+      <div className='projectList__title-div'>
+        <div className='projectList__cell'>
           <h3 className='projectList__title'>Name</h3>
-          <h3 className='projectList__title'>Key</h3>
-          <h3 className='projectList__title'>Type</h3>
-          <h3 className='projectList__title'>Lead</h3>
-          <h3 className='projectList__title'>Status</h3>
-          <h3 className='projectList__title'>Actions</h3>
         </div>
-          {projectList && projectList.map((project) => {
-              return <Project project={project} key={project.id}/>
-          })}
+        <div className='projectList__cell'>
+          <h3 className='projectList__title'>Key</h3>
+        </div>
+        <div className='projectList__cell'>
+          <h3 className='projectList__title'>Type</h3>
+        </div>
+        <div className='projectList__cell'>
+          <h3 className='projectList__title'>Lead</h3>
+        </div>
+        <div className='projectList__cell'>
+          <h3 className='projectList__title'>Status</h3>
+        </div>
+        <h3 className='projectList__title'>Actions</h3>
+      </div>
+      {projectList && projectList.map((project) => {
+        return <Project project={project} key={project.id} />
+      })}
     </div>
   )
 }

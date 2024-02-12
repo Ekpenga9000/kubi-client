f21a897d16123248ab6efbe89cddb712af918a83
Louis_Ekpenga <ekpenga9000@gmail.com>
2024-02-12 12:07:29 -0700

    Edit padding on issues list and sprint list.

--- src/components/project-details-dashboard-bottom/DashboardBottom.jsx
+++ src/components/project-details-dashboard-bottom/DashboardBottom.jsx
@@ -8,7 +8,15 @@ import AddIssueForm from "../addIssueForm/AddIssueForm";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import NoIssue from "../noIssue/NoIssue";
-import { DndContext, KeyboardSensor, PointerSensor, TouchSensor, closestCorners, useSensor, useSensors } from "@dnd-kit/core";
+import {
+  DndContext,
+  KeyboardSensor,
+  PointerSensor,
+  TouchSensor,
+  closestCorners,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
 import { arrayMove, sortableKeyboardCoordinates } from "@dnd-kit/sortable";
 import { draggableGetItemPos } from "../../utils/draggable/Draggable";
 // import { SortableContext, verticalListSortingStrategy } from "@dnd-kit/sortable";
@@ -18,7 +26,7 @@ const DashboardBottom = () => {
   const [issues, setIssues] = useState(null);
   const [addIssue, setAddIssue] = useState(false);
   const [issueLength, setIssueLength] = useState(0);
-  const [activateCreateBtn, setActivateCreateBtn] = useState(false); 
+  const [activateCreateBtn, setActivateCreateBtn] = useState(false);
 
   const url = import.meta.env.VITE_SERVER_URL;
   const token = sessionStorage.getItem("token");
@@ -42,82 +50,83 @@ const DashboardBottom = () => {
     }
   };
 
-
   useEffect(() => {
     fetchAllIssues();
   }, []);
 
-
-  const handleDragEnd = e => {
+  const handleDragEnd = (e) => {
     const { active, over } = e;
-    
+
     if (active.id === over.id) return;
 
-    setIssues(issues => {
+    setIssues((issues) => {
       const originalPos = draggableGetItemPos(active.id, issues);
       const newPos = draggableGetItemPos(over.id, issues);
 
       return arrayMove(issues, originalPos, newPos);
-    })
+    });
   };
 
   const sensors = useSensors(
     useSensor(PointerSensor),
     useSensor(TouchSensor),
     useSensor(KeyboardSensor, {
-      coordinateGetter: sortableKeyboardCoordinates
+      coordinateGetter: sortableKeyboardCoordinates,
     })
   );
-  
+
   return (
     <section className="dashboard-bottom">
-      <DndContext onDragEnd={handleDragEnd} sensors={sensors} collisionDetection={closestCorners}>
-
-      <div className="dashboard-bottom__container">
-        <div className="dashboard-bottom__left">
-          <div className="dashboard-bottom__title-container">
-            <div className="dashboard-bottom__title-div">
-              <h3 className="dashboard-bottom__title">
-                <LiaCubesSolid />
-                Backlogs
-              </h3>
-              <p className="dashboard-bottom__issues-count">{issueLength} {issueLength === 1 ? "issue" : "issues"}</p>
-            </div>
-            <div className="dashboard-bottom__title-div">
-            <p className="dashboard-bottom__estimate">Estimate 0</p>
-              {/* {(issueLength > 0 && !activateCreateBtn)&& <button className="dashboard-bottom__btn--create" onClick={createSprint}><LuCalendarPlus /> Create Sprint</button>}
-              {(issueLength === 0 || activateCreateBtn) && <button className="dashboard-bottom__btn--disabled"><LuCalendarPlus /> Create Sprint</button>} */}
-            </div>
-          </div>
-          <div className="dashboard-bottom__issues">
-            {issueLength === 0 && (
-              <div className="dashboard-bottom__empty">
-                <NoIssue/>
+      <DndContext
+        onDragEnd={handleDragEnd}
+        sensors={sensors}
+        collisionDetection={closestCorners}
+      >
+        <div className="dashboard-bottom__container">
+          <div className="dashboard-bottom__left">
+            <div className="dashboard-bottom__title-container">
+              <div className="dashboard-bottom__title-div">
+                <h3 className="dashboard-bottom__title">
+                  <LiaCubesSolid />
+                  Backlogs
+                </h3>
+                <p className="dashboard-bottom__issues-count">
+                  {issueLength} {issueLength === 1 ? "issue" : "issues"}
+                </p>
               </div>
-            )}
-            {issueLength > 0 && (
-              <div className="dashboard-bottom__issues-list">
-                <IssueList issues={issues} />
+              <div className="dashboard-bottom__title-div">
+                <p className="dashboard-bottom__estimate">Estimate 0</p>
               </div>
+            </div>
+            <div className="dashboard-bottom__issues">
+              {issueLength === 0 && (
+                <div className="dashboard-bottom__empty">
+                  <NoIssue />
+                </div>
+              )}
+              {issueLength > 0 && (
+                <div className="dashboard-bottom__issues-list">
+                  <IssueList issues={issues} />
+                </div>
+              )}
+            </div>
+            {!addIssue && (
+              <button
+                className="dashboard-bottom__btn--create"
+                onClick={handleAddIssue}
+              >
+                <IoAdd /> Create Issue
+              </button>
+            )}
+            {addIssue && (
+              <AddIssueForm
+                handleAddIssue={handleAddIssue}
+                fetchAllIssues={fetchAllIssues}
+              />
             )}
           </div>
-          {!addIssue && (
-            <button
-            className="dashboard-bottom__btn--create"
-            onClick={handleAddIssue}
-            >
-              <IoAdd /> Create Issue
-            </button>
-          )}
-          {addIssue && (
-            <AddIssueForm
-            handleAddIssue={handleAddIssue}
-            fetchAllIssues={fetchAllIssues}
-            />
-            )}
         </div>
-      </div>
-           </DndContext>
+      </DndContext>
     </section>
   );
 };
--- src/components/project-details-dashboard-top/DashboardTop.jsx
+++ src/components/project-details-dashboard-top/DashboardTop.jsx
@@ -8,26 +8,28 @@ import { IoIosPlay } from "react-icons/io";
 import { IoCloseOutline } from "react-icons/io5";
 import { FiEdit3 } from "react-icons/fi";
 import { MdDeleteOutline } from "react-icons/md";
+import { IoAddSharp } from "react-icons/io5";
 import IssueList from "../issue-list/IssueList";
 import plan from "../../assets/images/plan.png";
 import gsap from "gsap";
 import DeleteSprintModal from "../deleteSprintModal/DeleteSprintModal";
 import EditSprintModal from "../editSprintModal/EditSprintModal";
 
-
 const DashboardTop = () => {
   const [hasIssues, setHasIssues] = useState(false);
   const [isOption, setIsOption] = useState(false);
-  const [isLoading, setIsLoading] = useState(true); 
+  const [isLoading, setIsLoading] = useState(true);
   const [noSprint, setNoSprint] = useState(false);
-  const [noSprintMsg, setNoSprintMsg] = useState("No sprints to display for this project")
+  const [noSprintMsg, setNoSprintMsg] = useState(
+    "No sprints to display for this project"
+  );
   const [name, setName] = useState("");
-  const [sprintNumber, setSprintNumber] = useState(null); 
-  const [editSprintModal, setEditSprintModal] = useState(false); 
+  const [sprintNumber, setSprintNumber] = useState(null);
+  const [editSprintModal, setEditSprintModal] = useState(false);
   const [deleteSprintModal, setDeleteSprintModal] = useState(false);
   const navigate = useNavigate();
   const option = useRef();
-  const comp = useRef();  
+  const comp = useRef();
   const { projectId } = useParams();
   const token = sessionStorage.getItem("token");
   const url = import.meta.env.VITE_SERVER_URL;
@@ -37,149 +39,189 @@ const DashboardTop = () => {
   }
   const fetchLastestSprint = async () => {
     try {
-      const { data: sprint } = await axios.get(`${url}/sprints/${projectId}/latest`, {
-        headers: {
-          Authorization: `Bearer ${token}`
+      const { data: sprint } = await axios.get(
+        `${url}/sprints/${projectId}/latest`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
         }
-      });
+      );
 
       if (!sprint.name) {
-        setNoSprint(true); 
+        setNoSprint(true);
         setIsLoading(false);
       } else {
         setSprintNumber(sprint.id);
         setName(sprint.name);
-        setIsLoading(false); 
+        setIsLoading(false);
       }
     } catch (err) {
       console.log(err);
     }
-  }
+  };
 
   const createSprint = async () => {
     try {
-        await axios.post(`${url}/sprints/${projectId}`, {}, {
-        headers: {
-          Authorization: `Bearer ${token}`
+      await axios.post(
+        `${url}/sprints/${projectId}`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
         }
-        })
+      );
       setNoSprint(false);
-      fetchLastestSprint(); 
+      fetchLastestSprint();
     } catch (err) {
-      console.log(err); 
+      console.log(err);
     }
-  }
+  };
 
   useLayoutEffect(() => {
-    let ctx = gsap.context(() => { 
+    let ctx = gsap.context(() => {
       gsap.from(option.current, {
-        duration: 0.4, 
+        duration: 0.4,
         opacity: 0,
-        ease:"power1.in"
-      })
+        ease: "power1.in",
+      });
     }, comp);
     return ctx.revert();
-  },[])
+  }, []);
 
   useEffect(() => {
     fetchLastestSprint();
   }, [projectId, url, token]);
-  
 
   const toggleOptions = () => {
     setIsOption(!isOption);
-  }
+  };
 
   const toggleDeleteSprintModal = () => {
     setDeleteSprintModal(!deleteSprintModal);
     setIsOption(false);
-  }
+  };
 
   const handleEditSprintModal = () => {
     setEditSprintModal(!editSprintModal);
-  }
+  };
 
   const editSprint = () => {
     handleEditSprintModal();
     toggleOptions();
-  }
-  
+  };
 
   return (
-  <>
+    <>
       {isLoading && <section>Loading...</section>}
-      {noSprint && <section className="dashboard-top__no-sprint">
-        <h5 className="dashboard-top__sprint-title--muted">{ noSprintMsg }</h5>
-        <button className="dashboard-top__btn--create" onClick={createSprint}><LuCalendarPlus /> Create Sprint</button>
-      </section>}
-      {name && <section className="dashboard-top">
-        <div className="dashboard-top__title-container" ref={comp}>
-          <div className="dashboard-top__title-div">
-            <h4 className="dashboard-top__sprint-title">{name}</h4>
-            <p className="dashboard-top__issues-title">0 issues</p>
-          </div>
-          <div className="dashboard-top__title-div--bottom">
-            {hasIssues && <button className="dashboard-top__btn--start">
-              <IoIosPlay /> Start sprint
-            </button>}
-          
-            {!hasIssues && <button className="dashboard-top__btn--disabled">
-              <IoIosPlay /> Start sprint
-            </button>}
+      {noSprint && (
+        <section className="dashboard-top__no-sprint">
+          <h5 className="dashboard-top__sprint-title--muted">{noSprintMsg}</h5>
+          <button className="dashboard-top__btn--create" onClick={createSprint}>
+            <LuCalendarPlus /> Create Sprint
+          </button>
+        </section>
+      )}
+      {name && (
+        <section className="dashboard-top">
+          <div className="dashboard-top__title-container" ref={comp}>
+            <div className="dashboard-top__title-div">
+              <h4 className="dashboard-top__sprint-title">{name}</h4>
+              <p className="dashboard-top__issues-title">0 issues</p>
+            </div>
+            <div className="dashboard-top__title-div--bottom">
+              {hasIssues && (
+                <button className="dashboard-top__btn--start">
+                  <IoIosPlay /> Start sprint
+                </button>
+              )}
 
+              {!hasIssues && (
+                <button className="dashboard-top__btn--disabled">
+                  <IoIosPlay /> Start sprint
+                </button>
+              )}
 
-            <button className="dashboard-top__btn--options" onClick={toggleOptions}>
-              {isOption ? <IoCloseOutline /> : <SlOptions />}
-            </button>
-                 
-            {isOption && <ul className="dashboard-top__options-menu" ref={option}>
-              <li className="dashboard-top__options-item" onClick={editSprint}> <FiEdit3 /> Edit sprint</li>
-              <li className="dashboard-top__options-item" onClick={toggleDeleteSprintModal}><MdDeleteOutline />Delete sprint</li>
-            </ul>}
+              <button
+                className="dashboard-top__btn--options"
+                onClick={toggleOptions}
+              >
+                {isOption ? <IoCloseOutline /> : <SlOptions />}
+              </button>
+
+              {isOption && (
+                <ul className="dashboard-top__options-menu" ref={option}>
+                  <li
+                    className="dashboard-top__options-item"
+                    onClick={editSprint}
+                  >
+                    {" "}
+                    <FiEdit3 /> Edit sprint
+                  </li>
+                  <li
+                    className="dashboard-top__options-item"
+                    onClick={toggleDeleteSprintModal}
+                  >
+                    <MdDeleteOutline />
+                    Delete sprint
+                  </li>
+                </ul>
+              )}
+            </div>
           </div>
-        </div>
-        <div className="dashboard-top__issues-div">
-          {hasIssues && <IssueList />}
-          {!hasIssues && (
-            <div className="dashboard-top__no-issues-container">
-              <div className="dashboard-top__no-issues">
-                <div className="dashboard-top__img-div">
-                  <img src={plan} alt="Man and woman planning a project in front of a planning board." className="dashboard-top__img" />
-                </div>
-                <div className="dashboard-top__msg">
-                  <h5>Planning your sprint</h5>
-                  <p className="dashboard-top__p">
-                    Agree as a team on what to work needs to be completed and
-                    assign these issue to the sprint.
-                  </p>
+          <div className="dashboard-top__issues-div">
+            {hasIssues && <IssueList />}
+            {!hasIssues && (
+              <div className="dashboard-top__no-issues-container">
+                <div className="dashboard-top__no-issues">
+                  <div className="dashboard-top__img-div">
+                    <img
+                      src={plan}
+                      alt="Man and woman planning a project in front of a planning board."
+                      className="dashboard-top__img"
+                    />
+                  </div>
+                  <div className="dashboard-top__msg">
+                    <h5>Planning your sprint</h5>
+                    <p className="dashboard-top__p">
+                      Agree as a team on what to work needs to be completed and
+                      assign these issue to the sprint.
+                    </p>
+                    <button className="dashboard-top__btn--create"> <IoAddSharp /> Add Issue</button>
+                  </div>
                 </div>
               </div>
-            </div>
-          )}
+            )}
+          </div>
+          <div className="dashboard-top__estimate-div">
+            <h4 className="dashboard-top__estimate">Estimate</h4>
+            <span>0</span>
+          </div>
+        </section>
+      )}
+      {editSprintModal && (
+        <div className="p-details__modal">
+          <EditSprintModal
+            handleEditSprintModal={handleEditSprintModal}
+            name={name}
+            sprintNumber={sprintNumber}
+          />
         </div>
-        <div className="dashboard-top__estimate-div">
-          <h4 className="dashboard-top__estimate">Estimate</h4>
-          <span>0</span>
+      )}
+      {deleteSprintModal && (
+        <div className="p-details__modal">
+          <DeleteSprintModal
+            name={name}
+            sprintNumber={sprintNumber}
+            toggleDeleteSprintModal={toggleDeleteSprintModal}
+            fetchLastestSprint={fetchLastestSprint}
+            setNoSprintMsg={setNoSprintMsg}
+            setName={setName}
+          />
         </div>
-      </section>}
-      {editSprintModal && <div className="p-details__modal">
-        <EditSprintModal
-          handleEditSprintModal={handleEditSprintModal}
-          name={name}
-          sprintNumber={sprintNumber}
-        />
-        </div>}
-      {deleteSprintModal && <div className="p-details__modal">
-        <DeleteSprintModal
-          name={name}
-          sprintNumber={sprintNumber}
-          toggleDeleteSprintModal={toggleDeleteSprintModal}
-          fetchLastestSprint={fetchLastestSprint}
-          setNoSprintMsg={setNoSprintMsg}
-          setName={setName}
-        />
-      </div>}
-  </> 
+      )}
+    </>
   );
 };
 
